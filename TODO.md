47d189e058a7e7f0ffc4b2041cbbc92d4558f58c
alissonbk <alissonbk2010@hotmail.com>
2025-03-14 11:25:41 -0300

    feat: cmdliner

---
+++ TODO.md
@@ -0,0 +1,1 @@
+### Improve the multiple option as it must have a list of "from's" and a list of "to's"
--- bin/main.ml
+++ bin/main.ml
@@ -1,61 +1,41 @@
-(* open Cmdliner *)
+open Cmdliner
 open Printf
 
 
-exception Invalid_parameter of string
-type cmd_options_t =
-  (* -r : Recursive -> enter in subdirectories*)
-  | Recursive
-  (* -i : Ignore -> should contain files or directories to be ignored, separated by comma if more than 1*)
-  | Ignore of string list
-  (* -m : Multiple -> rename multiple at the same time *)
-  | Multiple of string list
-  (* LastParam -> default and will be used when the option Multiple is not beeing used *)
-  | LastParam of string
+let renamer recursive ignore multiple from_word to_word =
+  let lst_to_string = List.fold_left (fun acc curr -> acc ^ "," ^ curr) "" in
+  printf "recursive: %b\nignore: %s\nmultiple: %s\nfrom_word: %s\nto_word: %s\n" 
+    recursive (lst_to_string ignore) (lst_to_string multiple) from_word to_word
+
+let recursive =
+  let doc = "Read sub directories recursively." in
+  Arg.(value & flag & info ["r"; "recursive"] ~docv:"Recursive" ~doc)
+
+let ignore =
+  let doc = "List of files to be ignored." in
+  Arg.(value & opt (list string) [] & info ["i"; "ignore"] ~docv:"Ignore" ~doc)
+
+let multiple =
+  let doc = "List of words when replacing multiple words at the same time (will run in parallel)" in
+  Arg.(value & opt (list string) [] & info ["m"; "multiple"] ~docv:"Multiple" ~doc)
+
+let from_word =
+  let doc = "The word pattern which will be replaced (can be written in any pattern as simpleExample or simple_exmaple ...)" in  
+  Arg.(value & pos 0 string "" & info [] ~docv:"from" ~doc)
+
+let to_word =
+  let doc = "The word that will replace the from word (can be written in any pattern as simpleExample or simple_exmaple ...)" in  
+  Arg.(value & pos 1 string "" & info [] ~docv:"to" ~doc)
+
+let renamer_term = Term.(const renamer $ recursive $ ignore $ multiple $ from_word $ to_word)
 
 
-(* retrieve comma separated parameters from options *)
-let rec read_option_parameters_r (lst : string list) (arg : string) (curr_idx : int) : string list =  
-  match (String.trim arg) with    
-    | s when String.contains s ',' -> 
-      let v = s |> String.split_on_char ',' |> List.hd in
-      read_option_parameters_r (v :: lst) Sys.argv.(curr_idx + 1) (curr_idx + 1)
-    | s when s <> "" -> (s :: lst)
-    | _ -> raise (Invalid_parameter "invalid option parameter \"\"")
-let read_option_parameters = read_option_parameters_r []   
-
-let retrieve_option_with_values arg curr_idx =    
-  match (String.trim arg) with
-    | "-r" -> Some Recursive
-    | "-i" -> 
-      let params = read_option_parameters Sys.argv.(curr_idx + 1) (curr_idx + 1) in
-      Some (Ignore params)
-    | "-m" -> 
-      let params = read_option_parameters Sys.argv.(curr_idx + 1) (curr_idx + 1) in
-      Some (Multiple params)
-    | _ -> None
-
-(* read the args *)
-let entrypoint () =    
-  printf "number of args: %d\n" (Array.length Sys.argv);
-  let last_pos = Array.length Sys.argv - 1 in  
-  let rec loop (lst : cmd_options_t list) (curr_idx : int) =        
-    match curr_idx with    
-    | n when n > last_pos -> lst
-    | n when n = last_pos -> loop (LastParam (String.trim Sys.argv.(n)) :: lst) (curr_idx + 1)
-    | _ ->            
-      (match retrieve_option_with_values Sys.argv.(curr_idx) curr_idx with
-        | None -> loop lst (curr_idx + 1)
-        | Some t -> loop (t :: lst) (curr_idx + 1)
-      )
+let info =
+  let doc = "Rename multiple words by pattern ex: someExample" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <hehey at example.org>." ]
   in
-  loop [] 1
+  Cmd.info "renamer" ~version:"%‌%VERSION%%" ~doc ~exits:Cmd.Exit.defaults ~man
 
-
-let () =
-  entrypoint () |> List.iter (function 
-    | Recursive -> printf "recursive"; printf "\n"
-    | Ignore lst -> printf "Ignore: \n\t"; lst |> List.iter (printf "%s - "); printf "\n"
-    | Multiple lst -> printf "Multiple: \n\t"; lst |> List.iter (printf "%s - "); printf "\n"
-    | LastParam lp -> printf "LastParam: \t%s" lp; printf "\n"
-  )
+let () = Stdlib.exit @@ Cmd.eval @@ Cmd.v info renamer_term
