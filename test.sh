f0abfc28a05dfdef2bbbd7fffb143449390bb662
alissonbk <alissonbk2010@hotmail.com>
2025-04-06 10:47:59 -0300

    test: update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-dune build; ./_build/install/default/bin/renamer -r -i "a, b" "fromTest" "toTest"
+dune build; cp _build/install/default/bin/renamer test_project; 
+pushd test_project; ./renamer -r -i "a, b" "textFile" "csvFile"; 
+rm renamer; popd
