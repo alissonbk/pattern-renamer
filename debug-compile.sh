6f18ac0ae060ea068f775078c9e7a31abc4f0ac5
alissonbk <alissonbk2010@hotmail.com>
2025-04-20 14:32:54 -0300

    build: script to debug

--- debug-compile.sh
+++ debug-compile.sh
@@ -1,14 +1,23 @@
 #!/bin/zsh
 
+# compile
 pushd lib;
 ocamlfind ocamlc -c types.ml;
 ocamlfind ocamlc -c utils.ml;
 ocamlfind ocamlc -c transform.ml;
 ocamlfind ocamlc -c file.ml;
-ocamlfind ocamlc -c core.ml;
-ocamlfind ocamlc -c command.ml;
+ocamlfind ocamlc -c core.mli;
+ocamlfind ocamlc -package cmdliner -linkpkg core.ml -c core.ml;
+ocamlfind ocamlc -package cmdliner -linkpkg command.ml -c command.ml;
 ocamlfind ocamlc -c renamer.ml;
-popd lib;
 
-ocamlfind ocamlc -package cmdliner -a lib/types.cmo lib/utils.cmo lib/core.cmo lib/command.cmo lib/renamer.cmo -o lib/renamer.cma
-ocamlfind ocamlc -package cmdliner -linkpkg lib/renamer.cma bin/main.ml -o main.debug
+ocamlfind ocamlc -package cmdliner -a types.cmo utils.cmo transform.cmo file.cmo core.cmo command.cmo renamer.cmo -o renamer.cma;
+ocamlfind ocamlc -g -package cmdliner,unix -linkpkg renamer.cma ../bin/main.ml -o ../main.debug;
+
+rm *.cm*;
+popd;
+
+# run the debugger
+ocamldebug main.debug -r -i "db.go" "textFile" "csvFile";
+
+rm main.debug;
