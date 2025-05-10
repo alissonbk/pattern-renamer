eeb2615457e22cc454b220a606d7f2ce0795e8cb
alissonbk <alissonbk2010@hotmail.com>
2025-05-10 15:41:07 -0300

    chore: update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 rm test_project/*.tmp; rm -rf _build;
 dune build; cp _build/install/default/bin/renamer test_project;
-pushd test_project; ./renamer --help;
+pushd test_project; ./renamer -r -i "README.md" "textFile, db" "csvFile, gb";
 yes | rm renamer; popd
