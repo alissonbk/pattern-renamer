357f7a87f14430a65d608062e9d47b25f26a08ce
alissonbk <alissonbk2010@hotmail.com>
2025-07-07 19:15:31 -0300

    fix: PKGBUILD removed dynamic pkgver()

--- update_aur.sh
+++ update_aur.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 DEST=~/projects/aur/pattern-renamer/
-sh bump-version.sh
+
+while true; do
+    read -rp "Bump version? [y/n]: " response
+    case "$response" in
+        [Yy]) 
+            sh bump-version.sh
+            break
+            ;;
+        [Nn])             
+            break
+            ;;
+        *) 
+            echo "Please enter y or n."
+            ;;
+    esac
+done
 makepkg --printsrcinfo > .SRCINFO
 cp PKGBUILD $DEST
 cp .SRCINFO $DEST
